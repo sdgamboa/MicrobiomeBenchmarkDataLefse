1d511cd3d812c0ef10ea936fd33218c95719bf86
sdgamboa <samuel.gamboa.tuz@gmail.com>
2024-08-22 16:24:18 -0400

    Add bash script to run lefse

--- script.sh
+++ script.sh
@@ -1,7 +1,13 @@
 
+
+
+lefse_format_input.py gingivalplaque.txt gingivalplaque.in -c 1 -s 2 -u 3 -o 1000000 
 i=1
 while [ $i -le 10 ]
 do
-    echo $i
+
+    bp=$(( 30 + ($i * 10 )))
+    echo Running iteration $i with bootstrap $bp...
+    lefse_run.py -a 0.1 -w 0.1 -l 0 -b $bp gingivalplaque.in gingivalplaque\_it"$i"\_bp"$bp".res
     i=$(( $i + 1 ))
 done
